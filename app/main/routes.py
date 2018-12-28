d8927654619e4c0c19139b8cc6b3052956188671
chemberlen <7379167@gmail.com>
2018-12-28 06:38:47 +0200

    Creat post page start rebuild

--- app/main/routes.py
+++ app/main/routes.py
@@ -93,20 +93,20 @@ def user(username):
     return render_template('user.html', user=user, posts=posts.items,next_url=next_url, prev_url=prev_url)
 
 
-@bp.route('/edit_profile', methods=['GET', 'POST'])
+@bp.route('/creat_post', methods=['GET', 'POST'])
 @login_required
-def edit_profile():
+def creat_post():
     form = EditProfileForm(current_user.username)
     if form.validate_on_submit():
         current_user.username = form.username.data
         current_user.about_me = form.about_me.data
         db.session.commit()
         flash(_('Your changes have been saved.'))
-        return redirect(url_for('main.edit_profile'))
+        return redirect(url_for('main.creat_post'))
     elif request.method == 'GET':
         form.username.data = current_user.username
         form.about_me.data = current_user.about_me
-    return render_template('edit_profile.html', title=_('Edit Profile'),
+    return render_template('creat_post.html', title=_('Creat new post'),
                            form=form)
 
 
